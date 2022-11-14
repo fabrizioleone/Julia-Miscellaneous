84722324d77ee57ac60106a0f4aadcd33d31f0c1
Fabrizio Leone <faleone@ulb.be>
2022-11-14 17:03:47 +0000

    Update system-nonlinear-equations-solver.jl

--- system-nonlinear-equations-solver.jl
+++ system-nonlinear-equations-solver.jl
@@ -1,5 +1,5 @@
 # Call Packages
-using Random, Optim, NLsolve
+using Random, NLsolve
 
 # Set seeds
 Random.seed!(1704)
@@ -10,37 +10,6 @@ x0   = ones(N)
 A    = rand(N, N)
 a    = [0.5, 0.4, 1.0]
 
-#--------------------------------------------------------#
-# Option 1: use Optim.jl
-#--------------------------------------------------------#
-
-# Define objective function: declare a scalar
-obj_fun(x) = 1.0
-
-# Define constraint
-function con_c!(c, x, a) 
-    c[1] = x[1]^2 - a[1]
-    c[2] = log(x[2]) - a[2]
-    c[3] = x[3]^3 - x[1] - a[3]
-end
-
-# Define constraints on x 
-lx   = fill(-Inf, N)
-ux   = fill(Inf, N)
-
-# Define constraints on c
-lc   = fill(0.0, N)
-uc   = fill(0.0, N)
-
-# Run optimization
-obj  = TwiceDifferentiable(obj_fun, x0)
-con  = TwiceDifferentiableConstraints((c,x) -> con_c!(c, x, a), lx, ux, lc, uc)
-res  = optimize(obj, con, x0, IPNewton(); autodiff = :forward)
-
-#--------------------------------------------------------#
-# Option 1: use NLsolve.jl
-#--------------------------------------------------------#
-
 # Define objective function
 function obj_fun!(F, x, a) 
     F[1] = x[1]^2 - a[1]
@@ -49,10 +18,10 @@ function obj_fun!(F, x, a)
 end
 
 # Run optimization
-res1 = nlsolve((F,x) -> obj_fun!(F, x, a), x0, autodiff = :forward)
+res = nlsolve((F,x) -> obj_fun!(F, x, a), x0, autodiff = :forward)
 
-#--------------------------------------------------------#
 # Display solution
-#--------------------------------------------------------#
-[res.minimizer res1.zero]
-#@show con_c!(fill(NaN, N), res.minimizer, a) 
+@show res.zero
+
+# For problems with constraints, see "Mixed complementarity problems"
+# https://github.com/JuliaNLSolvers/NLsolve.jl 
